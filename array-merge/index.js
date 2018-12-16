10f404900bc3d4faf3417b13746b9936b1fe602a
shangdev <609926202@qq.com>
2018-12-16 13:01:51 +0800

    Improve array-merge.

--- array-merge/index.js
+++ array-merge/index.js
@@ -1,15 +1,17 @@
 /**
- * Merge another array.
+ * Merge another array (without removing duplicates).
  * 
- * @example merge(['1', '2'], ['3']) // ['1', '2', '3']
+ * @example merge(['1', '2'], ['1', '3']) // ['1', '2', '3']
+ * @example merge([{a: 1}, {b: 2}], [{b: 2}, {c: 3}]) // [{a: 1}, {b: 2}, {b: 2}, {c: 3}]
  * 
- * @param {array} arr 
- * @param {array} args 
+ * @param {array} arr1
+ * @param {array} arr2
  */
-function merge(arr, args) {
-    if (!Array.isArray(arr) || !Array.isArray(args)) {
+function merge(arr1, arr2) {
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
         throw new Error('Expected arr is an array');
     }
 
-    return arr.concat(args);
+    //ES5 return arr1.concat(arr2);
+    return [...arr1, ...arr2];
 }
